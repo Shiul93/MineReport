32f4b65f1b7fd7c612f09e64db03934dd680356e
Shiul93 <l.f.llamas@udc.es>
2012-11-10 17:40:01 +0100

    Added commandMethods class with 2 implemented command methods.

--- src/com/github/Shiul93/MineReporter/MineReporter.java
+++ src/com/github/Shiul93/MineReporter/MineReporter.java
@@ -3,11 +3,12 @@ package com.github.Shiul93.MineReporter;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
-
+import java.util.*;
 public final class MineReporter extends JavaPlugin {
 	
 	@Override
     public void onEnable(){
+		List<Report> reportList = new ArrayList<Report>();
 		getLogger().info("MineReporter enabled");
     }
 	
--- src/com/github/Shiul93/MineReporter/Report.java
+++ src/com/github/Shiul93/MineReporter/Report.java
@@ -38,4 +38,5 @@ public String toString(){
 	
 	
 }
+
 }
